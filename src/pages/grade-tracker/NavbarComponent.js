42df56ead74429cc85f1b26e46863bb90a000b52
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-02-05 16:35:19 +0700

    Fixed selectedCourse bug & Created NavbarComponent

---
+++ src/pages/grade-tracker/NavbarComponent.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Modal, Button, Navbar, Nav, NavDropdown, Form } from "react-bootstrap"
+import * as x from "./index" 
+
+export default function NavbarComponent({handleSetSelectedMajor}) {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand className="my-margin app-title" href="#grade-tracker">
+        Grade Tracker
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className="my-margin" id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Curriculum</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+        </Nav>
+        <Nav>
+          <NavDropdown title="Select Major" id="basic-nav-dropdown">
+            <NavDropdown.Item
+              href="#computer-science"
+              onClick={() => {
+                handleSetSelectedMajor("CS")
+              }}
+            >
+              Computer Science
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              href="#IT"
+              onClick={() => {
+                handleSetSelectedMajor("IT")
+              }}
+            >
+              IT
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
