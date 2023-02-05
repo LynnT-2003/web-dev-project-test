45e5fe04af5f7a28a19650d0cf4516aab3cb15e9
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2023-02-05 18:16:53 +0700

    Optimized functions and imported Components

--- src/pages/grade-tracker/index.js
+++ src/pages/grade-tracker/index.js
@@ -2,9 +2,13 @@ import * as React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Modal, Button, Navbar, Nav, NavDropdown, Form } from "react-bootstrap"
 import { useLocalStorage } from "react-use"
-// import css
 import "./style.css"
 import NavbarComponent from "./NavbarComponent"
+import GreetingComponent from "./GreetingComponent"
+import AccumulativeGPA from "./AccumulativeGPA"
+import ProgressListComponent from "./ProgressListComponent"
+import FormSelectMajorComponent from "./FormSelectMajorComponent"
+import CurriculumComponent from "./CurriculumComponent"
 
 function Page() {
 
@@ -36,6 +40,21 @@ function Page() {
 
 	// useState for a selected course
 	const [selectedCourse, setSelectedCourse] = React.useState([])
+  const handleSetSelectedCourse = (course) => {
+    setSelectedCourse(course)
+  }
+
+	// useState for a selected course code 
+	const [selectedCourseCode, setSelectedCourseCode] = React.useState([])
+  const handleSetSelectedCoursecode = (code) => {
+    setSelectedCourse(code)
+  }
+
+	// useState for a selected course credit
+	const [selectedCourseCredit, setSelectedCourseCredit] = React.useState([])
+  const handleSetSelectedCourseCredit = (credit) => {
+    setSelectedCourse(credit)
+  }
 
 	// useState for selectedGrade
 	const [selectedGrade, setSelectedGrade] = React.useState([])
@@ -57,6 +76,10 @@ function Page() {
 
   const handleClickSetSelectedMajor = (major) => {
     setSelectedMajor(major)
+  }
+  
+  const handleClickSetSelectedGroup = (group) => {
+    setSelectedGroup(group)
   }
 
 	// function for adding to gradeList (a neccessity to gradeList and setGradeList)
@@ -175,88 +198,41 @@ function Page() {
 	}, [selectedMajor])
 
 	return (
-		<>
+    <>
+      <NavbarComponent handleSetSelectedMajor={handleClickSetSelectedMajor} />
 
-    <NavbarComponent handleSetSelectedMajor={handleClickSetSelectedMajor}/>
-
-
-
-			<div>
-
-				<div className="main-body">
-					<div className="welcome-text">
-						Hello Mr. Lynn Thit !<br></br> Welcome to the 2023 Curriculum for{" "}
-						{selectedMajor}
-					</div>
-				</div>
-
-				<h2 className="main-title">Overall GPA: {(totalPoints / totalCredits).toFixed(2)}</h2>
-				<h2 className="main-title">Total points: {totalPoints} Total credits: {totalCredits}</h2>
-				<table className="table table-striped">
-					<thead>
-						<tr>
-							<th>Course Name</th>
-							<th>Course Code</th>
-							<th>Course Credits</th>
-							<th>Course Grade</th>
-						</tr>
-					</thead>
-					<tbody>
-						{/* {selectedCourse.course && selectedCourse.grade && (
-              <tr>
-                <td>{selectedCourse.course}</td>
-                <td>{selectedCourse.grade}</td>
-              </tr>
-            )} */}
-						{gradeList.map((item, i) => {
-							return (
-								<tr key={i}>
-									<td>{item[0]}</td>
-									<td>{item[1]}</td>
-									<td>{item[2]}</td>
-									<td>{item[3]}</td>
-								</tr>
-							)
-						})}
-					</tbody>
-				</table>
+      <div className="Container">
+				<div className="main-body welcome-text">
+          <GreetingComponent major={selectedMajor}/>
+          <AccumulativeGPA totalCredits = {totalCredits} totalPoints = {totalPoints}/>
+        </div> <ProgressListComponent list={gradeList}/>
 			</div>
 
+      <FormSelectMajorComponent
+        selectedGroup = {selectedGroup}
+        setSelectedGroup = {handleClickSetSelectedGroup}
+        subjectsData = {subjectsData}
+      />
 
+      {/* <CurriculumComponent
+        selectedGroup = {selectedGroup}
+        handleClickGrade = {handleClickGrade}
+        setSelectedCourse = {handleSetSelectedCourse}
+        handleClickShowModal = {handleClickShowModal}
+        showModal = {showModal}
+        handleCloseModal = {handleCloseModal}
+        selectedYear = {selectedYear}
+        selectedSemester = {selectedSemester}
+        handleSelectYear = {handleSelectYear}
+        handleSelectSemester = {handleSelectSemester}
+        handleClick = {handleClick}
+        selectedCourse = {selectedCourse}
+        setSelectedCourseCode = {handleSetSelectedCoursecode}
+        setSelectedCourseCredit = {handleSetSelectedCourseCredit}
+        selectedCourseCode = {selectedCourseCode}
+        selectedCourseCredit = {selectedCourseCredit}
 
-			<table className="table" style={{ border: "white" }}>
-				<tbody>
-					<tr>
-						<td
-							style={{
-								width: "270px",
-								fontFamily: "Georgia",
-								fontSize: "20px",
-							}}>Please select a course group:</td>
-						<td style={{
-							textAlign: "left"
-						}}>
-							<Form.Select
-								aria-label="Select Group"
-								value={selectedGroup.groupName}
-								onChange={e =>
-									setSelectedGroup(
-										subjectsData.find(group => group.groupName === e.target.value)
-									)
-								}
-							>
-								{subjectsData.map(group => (
-									<option key={group.groupName} value={group.groupName}>
-										{group.groupName}
-									</option>
-								))}
-							</Form.Select>
-						</td>
-						<td>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+      /> */}
 
 			<table className="table table-striped">
 				<thead>
@@ -282,72 +258,72 @@ function Page() {
 
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("A") && setSelectedGrade("A")}
+										onClick={() => handleClickGrade("A")}
 									>
 										A
 									</button>
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("A-") && setSelectedGrade("A-")}
+										onClick={() => handleClickGrade("A-")}
 									>
 										A-
 									</button>
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("B+") && setSelectedGrade("B+")}
+										onClick={() => handleClickGrade("B+")}
 									>
 										B+
 									</button>
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("B") && setSelectedGrade("B")}
+										onClick={() => handleClickGrade("B")}
 									>
 										B
 									</button>
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("B-") && setSelectedGrade("B-")}
+										onClick={() => handleClickGrade("B-")}
 									>
 										B-
 									</button>
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("C+") && setSelectedGrade("C+")}
+										onClick={() => handleClickGrade("C+")}
 									>
 										C+
 									</button>
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("C") && setSelectedGrade("C")}
+										onClick={() => handleClickGrade("C")}
 									>
 										C
 									</button>
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("C-") && setSelectedGrade("C-")}
+										onClick={() => handleClickGrade("C-")}
 									>
 										C-
 									</button>
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("D") && setSelectedGrade("D")}
+										onClick={() => handleClickGrade("D")}
 									>
 										D
 									</button>
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("F") && setSelectedGrade("F")}
+										onClick={() => handleClickGrade("F")}
 									>
 										F
 									</button>
 									<button
 										className="button1 font-weight-bold"
-										onClick={() => handleClickGrade("W") && setSelectedGrade("W")}
+										onClick={() => handleClickGrade("W")}
 									>
 										W
 									</button>
 									<button
-										onClick={() => {setSelectedCourse(course.name); handleClickShowModal()}
+										onClick={() => {setSelectedCourse(course.name); setSelectedCourseCode(course.code); setSelectedCourseCredit(course.credit); handleClickShowModal()}
 										}
 										style={{ marginLeft: "10px" }}
 									>
@@ -389,7 +365,7 @@ function Page() {
 											</div>
 										</Modal.Body>
 										<Modal.Footer>
-											<Button variant="primary" onClick={() => {handleClick(selectedCourse, course.code, course.credit, selectedSemester, selectedYear); handleCloseModal()}}>
+											<Button variant="primary" onClick={() => {handleClick(selectedCourse, selectedCourseCode, selectedCourseCredit, selectedSemester, selectedYear); handleCloseModal()}}>
 												Done
 											</Button>
 										</Modal.Footer>
